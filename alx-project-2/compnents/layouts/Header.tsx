b66b53f7ac5f55ab30eefc7b41ff1f4c347fc607
Jefferson Archibald <archibaldjefferson17@gmail.com>
2025-11-22 17:02:10 +0000

    up

---
+++ alx-project-2/compnents/layouts/Header.tsx
@@ -0,0 +1,8 @@
+const Header =() => {
+    return(
+        <header className="p-4 bg-gray-200 text-gray-800">
+            <h1 className="text-xl font-bold">ALX PROJECT 2</h1>
+        </header>
+    )
+}
+export default Header;
