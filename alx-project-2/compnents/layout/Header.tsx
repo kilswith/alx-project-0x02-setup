c2cedb02ff7cf40239e8a55d1a68bc2a07486661
Jefferson Archibald <archibaldjefferson17@gmail.com>
2025-11-23 17:13:30 +0000

    up

--- alx-project-2/compnents/layout/Header.tsx
+++ alx-project-2/compnents/layout/Header.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 const Header =() => {
     return(
         <header className="p-4 bg-gray-200 text-gray-800">
             <h1 className="text-xl font-bold">ALX PROJECT 2</h1>
+
+            <nav className="flex gap-4 ml-6">
+                <link href="About"></link>
+            </nav>
         </header>
     )
 }
---
+++ alx-project-2/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <div className="p-6">
+            <h1 className="text-3xl font-bold">Home</h1>
+            <p>Welcome to the home Page!</p>
+        </div>
+    )
+}
